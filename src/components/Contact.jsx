3394aa49a0213908730418bd33953a530b73ee98
RichCode <stanleyogada2000@gmail.com>
2023-05-30 22:17:43 +0100

    Have Contact section working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,7 +15,11 @@ const Contact = () => {
     message: "",
   });
 
-  const [loading, setLoading] = useState(false);
+  const [state, setState] = useState({
+    loading: false,
+    success: false,
+    error: false,
+  });
 
   const handleChange = (e) => {
     const { target } = e;
@@ -29,7 +33,11 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true);
+    setState({
+      loading: true,
+      success: false,
+      error: false,
+    });
 
     emailjs
       .send(
@@ -37,18 +45,20 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: `${form.name}:: RICH_CODE OFFICIAL WEBSITE!!`,
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
-          message: form.message,
+          to_email: import.meta.env.VITE_APP_MY_EMAIL_ADDRESS,
+          message: `${form.message} \n\n FROM: ${form.email}`,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
+          setState({
+            loading: false,
+            success: true,
+            error: false,
+          });
           setForm({
             name: "",
             email: "",
@@ -56,21 +66,79 @@ const Contact = () => {
           });
         },
         (error) => {
-          setLoading(false);
+          setLoading({ loading: false, success: false, error: true });
           console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
         }
       );
   };
 
+  const renderAlert = (() => {
+    if (state.success)
+      return (
+        <div
+          class="fixed z-10 top-[100px] left-[50px] max-w-xs bg-white border rounded-md shadow-lg dark:bg-gray-800 dark:border-gray-700"
+          role="alert"
+        >
+          <div class="flex p-4">
+            <div class="flex-shrink-0">
+              <svg
+                class="h-4 w-4 text-green-500 mt-0.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+              </svg>
+            </div>
+            <div class="ml-3">
+              <p class="text-sm text-gray-700 dark:text-gray-400">
+                Thank you. I will get back to you as soon as possible.
+              </p>
+            </div>
+          </div>
+        </div>
+      );
+
+    if (state.error)
+      return (
+        <div
+          class="fixed z-10 top-[100px] left-[50px] max-w-xs bg-white border rounded-md shadow-lg dark:bg-gray-800 dark:border-gray-700"
+          role="alert"
+        >
+          <div class="flex p-4">
+            <div class="flex-shrink-0">
+              <svg
+                class="h-4 w-4 text-red-500 mt-0.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+              </svg>
+            </div>
+            <div class="ml-3">
+              <p class="text-sm text-gray-700 dark:text-gray-400">
+                Ahh! something went wrong. Please try again.
+              </p>
+            </div>
+          </div>
+        </div>
+      );
+  })();
+
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+      className={`relative xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
+      {renderAlert}
+
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -78,54 +146,55 @@ const Contact = () => {
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className="mt-12 flex flex-col gap-8"
         >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
             <input
-              type='text'
-              name='name'
+              type="text"
+              name="name"
               value={form.name}
               onChange={handleChange}
               placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
             <input
-              type='email'
-              name='email'
+              type="email"
+              name="email"
               value={form.email}
               onChange={handleChange}
               placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
               rows={7}
-              name='message'
+              name="message"
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              placeholder="What you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
 
           <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+            disabled={!form.message || !form.name || !form.email}
           >
-            {loading ? "Sending..." : "Send"}
+            {state.loading ? "Sending..." : "Send"}
           </button>
         </form>
       </motion.div>
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
         <EarthCanvas />
       </motion.div>
